797a7be28faa76be74df1df2fb6776a1b0e15dce
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-22 18:53:56 -0300

    exercicio resolvido - DevSuperior

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,28 @@ public class Program {
 
 		int m, n;
 		
-		System.out.print("");
+		System.out.print("Qual a quantidade de linhas da matriz? ");
+		m = sc.nextInt();
+		System.out.print("Qual a quantidade de colunas da matriz? ");
+		n = sc.nextInt();
+		
+		int[][] matriz = new int[m][n]; // Criando uma matriz bi-dimensional
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				System.out.printf("Elemento [%d,%d]: ",i,j);
+				matriz[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("VALORES NEGATIVOS:");
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				if(matriz[i][j] < 0) {
+					System.out.println(matriz[i][j]);
+				}
+			}
+		}
 		
 		sc.close();
 	}
